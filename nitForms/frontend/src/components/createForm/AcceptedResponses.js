77f583cf4db47edf1bca281ab0efed72ed79a2c1
Jatinkundra <jatinkundra26@gmail.com>
2020-07-08 05:36:52 +0530

    accepted response change

--- nitForms/frontend/src/components/createForm/AcceptedResponses.js
+++ nitForms/frontend/src/components/createForm/AcceptedResponses.js
@@ -1,25 +1,25 @@
 import React, { Component, Fragment } from "react";
 import AutoComplete from "./AutoComplete";
-
+import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic/build/ckeditor";
-import Mention from "@ckeditor/ckeditor5-mention/src/mention";
-ClassicEditor.create(editorElement, {
-  plugins: [Mention],
-  mention: {
-    feeds: [
-      {
-        marker: "@",
-        feed: ["@Barney", "@Lily", "@Marshall", "@Robin", "@Ted"],
-      },
-    ],
-  },
-})
-  .then((editor) => {
-    console.log("Editor was initialized", editor);
-  })
-  .catch((error) => {
-    console.error(error.stack);
-  });
+// import Mention from "@ckeditor/ckeditor5-mention/src/mention";
+// ClassicEditor.create(editorElement, {
+//   plugins: [Mention],
+//   mention: {
+//     feeds: [
+//       {
+//         marker: "@",
+//         feed: ["@Barney", "@Lily", "@Marshall", "@Robin", "@Ted"],
+//       },
+//     ],
+//   },
+// })
+//   .then((editor) => {
+//     console.log("Editor was initialized", editor);
+//   })
+//   .catch((error) => {
+//     console.error(error.stack);
+//   });
 
 export class AcceptedResponses extends Component {
   onChange = (event, editor) => {
@@ -42,6 +42,16 @@ export class AcceptedResponses extends Component {
         />
 
         <h2>Using CKEditor 5 build in React</h2>
+        <CKEditor
+          editor={ClassicEditor}
+          data=""
+          onInit={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            const data = editor.getData();
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={this.onChange(event, this.editor)}
+        />
       </Fragment>
     );
   }
